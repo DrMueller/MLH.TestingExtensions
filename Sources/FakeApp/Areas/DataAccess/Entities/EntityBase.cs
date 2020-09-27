c89c176ae76a657e6f986198d97d574a6474215c
Matthias Müller <matthias.mueller@trivadis.com>
2020-09-27 15:21:19 +0200

    WIP

--- Sources/FakeApp/Areas/DataAccess/Entities/EntityBase.cs
+++ Sources/FakeApp/Areas/DataAccess/Entities/EntityBase.cs
@@ -1,5 +1,8 @@
-﻿namespace Mmu.Mlh.TestingExtensions.FakeApp.Areas.DataAccess.Entities
+﻿using JetBrains.Annotations;
+
+namespace Mmu.Mlh.TestingExtensions.FakeApp.Areas.DataAccess.Entities
 {
+    [PublicAPI]
     public class EntityBase
     {
         public long? Id { get; set; }
